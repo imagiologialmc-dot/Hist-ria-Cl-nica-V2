542ac82bcbd47912f7b881fff6e861ef94475865
imagiologialmc-dot <imagiologialmc@gmail.com>
2026-04-25 14:31:46 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'aliva-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.jpg',
+  './icon-512.jpg',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
